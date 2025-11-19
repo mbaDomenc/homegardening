7bd86443a78b0314e82e9461549a50287e4e19cb
Domenico <d.dambrosio6@studenti.poliba.it>
2025-11-19 20:36:01 +0100

    Delete backend directory

--- backend/routers/weatherRouter.py
+++
@@ -1,9 +0,0 @@
-# backend/routes/weather_router.py
-from fastapi import APIRouter, Query
-from controllers.weather_controller import get_weather_by_city
-
-router = APIRouter(prefix="/api", tags=["weather"])
-
-@router.get("/weather")
-async def weather(city: str = Query(..., description="Es: Bari, IT")):
-    return await get_weather_by_city(city)
